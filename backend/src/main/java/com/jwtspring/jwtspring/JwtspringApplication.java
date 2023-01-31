357feedd0d587ee6a51c39858d435baaf7be3abb
Anisha Prajapati <anishaprajapati1004@gmail.com>
2023-01-31 23:36:54 +0530

    backend

---
+++ backend/src/main/java/com/jwtspring/jwtspring/JwtspringApplication.java
@@ -0,0 +1,13 @@
+package com.jwtspring.jwtspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtspringApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/jwtspring/jwtspring/JwtspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jwtspring.jwtspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
